50df5387e669d0eb5519934eb13f856f56f2d965
cptfalcn <54957544+cptfalcn@users.noreply.github.com>
2024-07-09 20:47:02 -0700

    Added Matlab code for reference

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,96 @@
 fn main() {
     println!("Hello, world!");
 }
+/* 
+function [y,TIME]=KapilaSimple(a,b,e,l_0,Tend,Delt)
+%Typical run kapila(1.0,0.5,1e-2,0.5,10,1e-2);
+%a is the first species amount
+%b controls z, the second species.  Modulated by epsilon
+%l_0 thermal conductivity
+%e is epsilon, the inverse reaction parameter. As it goes to zero more
+%explosive
+L=l_0*exp(1/e);
+z=e*b; %for stiffest use z=5e-3;
+y0=[1,a,z]; % theta(0),y(0),z(0)
+y=y0;
+
+F=@(t,y)[rhs(t,y,e,L)]';
+Fepic = @(y) F(y(end),y')';
+
+NumSteps=round(Tend/Delt);
+options=odeset('RelTol', 1e-10,'AbsTol',1e-10);
+EpicOption.N_step=1;
+EpicOption.RHS_function=F;
+h=Delt;
+start=tic;
+for i=1:NumSteps
+%t=y(i,end);
+yIn=y(i,:);
+%[tt,yUp]=ode15s(F, [yIn(end),yIn(end)+h],yIn,options);
+epic(Exp_Int_Leja,Fepic,[y(end),y(end)+h],y',EpicOption);
+yUp=yUp(end,:);
+for j=1:3
+    if(yUp(j)<0)
+        yUp(j)=0;
+    end
+end
+y(i+1,:)=yUp;
+  fprintf(['Completed time step:', num2str( (i)*h), '\n'])
+end
+TIME=toc(start);
+y=y';
+time=0:h:Tend;
+figure(1)
+plot(time,y,"LineWidth",1.5,"MarkerSize",15)
+xlabel("Time")
+ylabel("Non-Dimensional quantity")
+title('states over time')
+legend({'Temp','y','z'});
+
+for i=1:length(y)
+   J=jac(y(end,i),y(1:3,i),e,L);
+   eig_(i,:)=sort(eig(J));
+end
+
+figure(2)
+semilogy(time,abs(real(eig_)),"LineWidth",1.5,"MarkerSize",15);
+title('real part of eigs over time')
+xlabel('time')
+ylabel('real part')
+legend({'E1','E2','E3'});
+
+%figure(3)
+%plot(time,imag(eig_));
+%xlabel('time')
+%ylabel('complex part')
+%title('complex part of eigs over time')
+%legend({'E1','E2','E3'});
+%%
+
+function yd=rhs(t,y,e,L)
+
+yd=zeros(size(y)-1);
+
+omega=L*y(2).*y(3).*exp(-1./(e*y(1)));
+yd(1)=y(3);
+yd(2)=-omega;
+yd(3)=-yd(2)-yd(1);
+
+
+% y = [theta,y,z]
+function J=jac(~,y,e,L)
+
+n=length(y);
+J=zeros(n,n);
+
+%omega=l*y(2).*y(3).*exp(-1./(e*y(1)));
+
+J(1,3)=1;
+J(2,1)=-(L*y(2)*y(3)*exp(-1/(e*y(1))))/(e*y(1)^2);
+J(2,2)=-L*y(3)*exp(-1/(e*y(1)));
+J(2,3)=-L*y(2)*exp(-1/(e*y(1)));
+J(3,1)=-J(2,1);
+J(3,2)=-J(2,2);
+J(3,3)=-J(2,3)-1;
+
+*/
